009ccbe3bc7a84978633c1dc97ea779e5c8f3f50
无尽愿 <adelphicoder@gmail.com>
2018-04-21 18:09:55 +0800

    Create token.go

---
+++ part5/interpreter/token.go
@@ -0,0 +1,29 @@
+package interpreter
+
+// Token types
+//
+// EOF (end-of-file) token is used to indicate that
+// there is no more input left for lexical analysis
+type TokenType = int
+
+const (
+	cTokenTypeOfNone TokenType = iota
+	cTokenTypeOfInteger
+	cTokenTypeOfPlusSign
+	cTokenTypeOfMinusSign
+	cTokenTypeOfMulSign
+	cTokenTypeOfDivSign
+	cTokenTypeOfEOF
+)
+
+type token struct {
+	t TokenType   // token type: INTEGER, PLUS, MINUS, MUL, DIV, or EOF
+	v interface{} // token value: non-negative integer value, '+', '-', '*', '/', or None
+}
+
+func newToken(t TokenType, v interface{}) token {
+	return token{
+		t: t,
+		v: v,
+	}
+}
