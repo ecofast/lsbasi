295efab5e64a3c35491f733afd0fb8edc8ffd8e5
无尽愿 <adelphicoder@gmail.com>
2018-04-21 18:09:05 +0800

    Create interpreter.go

---
+++ part5/interpreter/interpreter.go
@@ -0,0 +1,74 @@
+package interpreter
+
+import (
+	"fmt"
+)
+
+type Interpreter struct {
+	lexer     *Lexer
+	currToken token
+}
+
+func New() *Interpreter {
+	return &Interpreter{}
+}
+
+// compare the current token type with the passed token type
+// and if they match then "eat" the current token
+// and assign the next token to the self.currToken,
+// otherwise raise an exception.
+func (self *Interpreter) eat(tokenType TokenType) {
+	if self.currToken.t == tokenType {
+		self.currToken = self.lexer.getNextToken()
+		return
+	}
+
+	panic(fmt.Sprintf("Error parsing input: %s", self.lexer.text))
+}
+
+// Return an INTEGER token value.
+func (self *Interpreter) factor() int {
+	ret := self.currToken
+	self.eat(cTokenTypeOfInteger)
+	return ret.v.(int)
+}
+
+// term: factor ((MUL | DIV) factor)*
+func (self *Interpreter) term() int {
+	ret := self.factor()
+	for self.currToken.t == cTokenTypeOfMulSign || self.currToken.t == cTokenTypeOfDivSign {
+		if self.currToken.t == cTokenTypeOfMulSign {
+			self.eat(cTokenTypeOfMulSign)
+			ret = ret * self.factor()
+			continue
+		}
+		self.eat(cTokenTypeOfDivSign)
+		ret = ret / self.factor()
+	}
+	return ret
+}
+
+/*
+Arithmetic expression parser / interpreter.
+
+	expr  : term ((PLUS | MINUS) term)*
+	term  : factor ((MUL | DIV) factor)*
+	factor: INTEGER
+*/
+func (self *Interpreter) Parse(s string) int {
+	self.lexer = newLexer([]rune(s))
+	// set current token to the first token taken from the input
+	self.currToken = self.lexer.getNextToken()
+
+	ret := self.term()
+	for self.currToken.t == cTokenTypeOfPlusSign || self.currToken.t == cTokenTypeOfMinusSign {
+		if self.currToken.t == cTokenTypeOfPlusSign {
+			self.eat(cTokenTypeOfPlusSign)
+			ret = ret + self.term()
+			continue
+		}
+		self.eat(cTokenTypeOfMinusSign)
+		ret = ret - self.term()
+	}
+	return ret
+}
