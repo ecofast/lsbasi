8fb9d7d963ccce52fb9df7624de17f4fbc9dbcd6
无尽愿 <adelphicoder@gmail.com>
2018-04-27 01:44:39 +0800

    Create main.go

---
+++ part9/main.go
@@ -0,0 +1,33 @@
+// part9 project main.go
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"part9/interpreter"
+	"strings"
+	"unsafe"
+)
+
+func main() {
+	fmt.Println("Let's Build A Simple Interpreter - Part 9")
+
+	interdivter := interpreter.New()
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("pls enter file name: ")
+		if s, err := reader.ReadString('\n'); err == nil {
+			fileName := strings.TrimSpace(s)
+			if b, err := ioutil.ReadFile(fileName); err == nil {
+				interdivter.Interpret(*(*string)(unsafe.Pointer(&b)))
+				interdivter.PrintSymbolTable()
+			} else {
+				fmt.Printf("cannot find file: %s\n", fileName)
+			}
+			continue
+		}
+		break
+	}
+}
